e4437d47a9b1393cc09acc7c657458cf4767c964
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-24 13:33:24 +0100

    clearTask function to clear all tasks completed

--- app.js
+++ app.js
@@ -7,27 +7,44 @@ const taskInput = document.querySelector("#task");
 //load all event listeners
 const loadEventListeners = () => {
   form.addEventListener("submit", addTask);
+  taskList.addEventListener("click", removeTask);
+  clearBtn.addEventListener("click", clearTasks);
+
 };
 
 const addTask = (e) => {
-    if (taskInput.value === ""){
-        alert('Add a task!');
-    }else{
-        const li = document.createElement("li");
-        li.className="collection-item";
-        li.appendChild(document.createTextNode(taskInput.value));
+  if (taskInput.value === "") {
+    alert("Write a title first!");
+  } else {
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    li.appendChild(document.createTextNode(taskInput.value));
 
-        //create a remove link
-        const link = document.createElement("a");
-        link.className = "delete-item secondary-content";
-        link.innerHTML = '<i class="fa fa-remove"></i>';
-        li.appendChild(link);
+    //create a remove link
+    const link = document.createElement("a");
+    link.className = "delete-item secondary-content";
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
 
-        taskList.appendChild(li);
-    }
+    taskList.appendChild(li);
+  }
 
-    e.preventDefault();
-
+  e.preventDefault();
 };
+
+const removeTask = (e) => {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+};
+
+const clearTasks = () => {
+  while (taskList.firstChild) {
+    taskList.firstChild.remove();
+  }
+};
+
 
 loadEventListeners();
