c8172f38769a26044f74866f35e225d3f6f3119a
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-28 16:43:21 +0100

    the UI to provention of user mistakes has been improved. the project is DONE.

--- loan-calculator/app.js
+++ loan-calculator/app.js
@@ -1,56 +1,63 @@
 const loadAllEvents = () => {
-  document
-    .getElementById("loan-form")
-    .addEventListener("submit", calculateResult);
+  document.getElementById("loan-form").addEventListener("submit", (e) => {
+    document.getElementById("results").style.display = "none";
+    if (!(amount.value && interest.value && years.value)) {
+      alert("Please fill out all fields to proceed");
+    } else {
+      document.getElementById("loading").style.display = "block";
+      setTimeout(calculateResult, Math.floor(Math.random() * 9 + 1) * 100);
+    }
+
+    e.preventDefault();
+  });
 };
 
-const calculateResult = (e) => {
+const calculateResult = () => {
   const amount = document.getElementById("amount");
   const interest = document.getElementById("interest");
   const years = document.getElementById("years");
   const monthlyPayment = document.getElementById("monthly-payment");
   const totalPayment = document.getElementById("total-payment");
   const totalInterest = document.getElementById("total-interest");
+  const CalculateButton = document.querySelector(".btn");
 
   const principal = parseFloat(amount.value);
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
 
-
   //compute monthly payment
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
   const monthly = (principal * x * calculatedInterest) / (x - 1);
- 
-  if (isFinite(monthly)){
 
-   // console.log(monthlyPayment.value)
+  if (isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
-  }else{
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
 
-    showError('Please check your numbers!')
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("results").style.display = "block";
+  } else {
+    showError("Please check your numbers!");
   }
-
- e.preventDefault();
 };
 
-const clearError = ()=>{
-    document.querySelector('.alert').remove()
-}
+const clearError = () => {
+  document.querySelector(".alert").remove();
+  document.querySelector(".btn").disabled = false;
+};
 
-const showError = (error) => { 
-    const errorDiv = document.createElement('div');
-    errorDiv.className= 'alert alert-danger';
-    errorDiv.appendChild(document.createTextNode(error));
+const showError = (error) => {
+  const errorDiv = document.createElement("div");
+  errorDiv.className = "alert alert-danger";
+  errorDiv.appendChild(document.createTextNode(error));
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+  card.insertBefore(errorDiv, heading);
+  document.getElementById("loading").style.display = "none";
 
-    const card = document.querySelector(".card");
-    const heading = document.querySelector(".heading");
-
-    card.insertBefore(errorDiv , heading);
-    setTimeout(clearError, 3000);
-}
-
-
+  // to prevent multi click on Calculate button
+  document.querySelector(".btn").disabled = true;
+  setTimeout(clearError, 3000);
+};
 
 loadAllEvents();
