b2c21ff67d074501f9cdf6c37bcfbf8e56084cfc
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-30 13:44:59 +0100

    making a 'play again' to refresh the page and reload the application. the app is done.

--- number-guesser/app.js
+++ number-guesser/app.js
@@ -1,6 +1,10 @@
+const getRandumNum = (min , max) => {
+    return Math.floor(Math.random() * (max - min + 1)+ min);
+}
+
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandumNum(min , max),
   guessesLeft = 3;
 
 const game = document.querySelector("#game"),
@@ -10,38 +14,60 @@ const game = document.querySelector("#game"),
   guessInput = document.querySelector("#guess-input"),
   message = document.querySelector(".message");
 
+minNum.textContent = min;
+maxNum.textContent = max;
 
-  minNum.textContent = min;
-  maxNum.textContent = max;
 
-const setMessage = (msg , color) => {
-    message.style.color = color;
-    message.textContent = msg;
+const setMessage = (msg, color) => {
+  message.style.color = color;
+  message.textContent = msg;
+};
+
+game.addEventListener("mousedown", (e) => {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
+const gameOver = (won, msg) => {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  message.style.color = color;
+  setMessage(msg);
+
+  //Play again
+  guessBtn.value = "Play Again";
+  guessBtn.className += "play-again";
+};
+
+const numberCheck = () => {
+  let enteredNum = parseInt(guessInput.value);
+  if (isNaN(enteredNum) || enteredNum < min || enteredNum > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
+  } else if (enteredNum === winningNum) {
+    //guessInput.disabled = true;
+    //guessInput.style.borderColor = "green";
+    gameOver(true, `${enteredNum} is true, you Won!`);
+    // setMessage(`${enteredNum} is true, you Won!`, "green");
+  } else {
+    guessesLeft -= 1;
+    if (guessesLeft === 0) {
+      gameOver(
+        false,
+        `Game Over, you lost! the correct number was ${winningNum}`
+      );
+    } else {
+      //   gameOver(false , `${enteredNum} is not correct! ${guessesLeft} guesses left` );
+      //   guessInput.disabled = false;
+      guessInput.focus();
+      guessInput.value = "";
+      setMessage(
+        `${enteredNum} is not correct! ${guessesLeft} guesses left`,
+        "red"
+      );
     }
-  const numberCheck = ()=>{
+  }
+};
 
-    let enteredNum = parseInt(guessInput.value);
-    if (isNaN(enteredNum) || enteredNum< min || enteredNum> max){
-        setMessage(`Please enter a number between ${min} and ${max}` , 'red')
-    }
-
-    if (enteredNum === winningNum){
-        guessInput.disabled = true;
-        guessInput.style.borderColor = 'green';
-        setMessage(`${enteredNum} is true, you Won!` , 'green');
-    }
-    else{
-        guessesLeft -=1;
-        if (guessesLeft === 0){
-            guessInput.disabled = true;
-        guessInput.style.borderColor = 'red';
-        setMessage(`Game Over, you lost! the correct number was ${winningNum}`);
-        }else{
-            guessInput.style.borderColor = 'red';
-            guessInput.value = '';
-            setMessage(`${enteredNum} is not correct! ${guessesLeft} guesses left` , 'red');
-        }
-    }
-}
-
-  guessBtn.addEventListener('click' , numberCheck)
+guessBtn.addEventListener("click", numberCheck);
