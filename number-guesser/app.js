96ea54df6666cbfc2036aca53b4b52ad9cc51f32
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-30 11:17:52 +0100

    the logic of application is completed

--- number-guesser/app.js
+++ number-guesser/app.js
@@ -18,11 +18,30 @@ const setMessage = (msg , color) => {
     message.style.color = color;
     message.textContent = msg;
     }
-  const getNumber = ()=>{
+  const numberCheck = ()=>{
+
     let enteredNum = parseInt(guessInput.value);
     if (isNaN(enteredNum) || enteredNum< min || enteredNum> max){
         setMessage(`Please enter a number between ${min} and ${max}` , 'red')
     }
+
+    if (enteredNum === winningNum){
+        guessInput.disabled = true;
+        guessInput.style.borderColor = 'green';
+        setMessage(`${enteredNum} is true, you Won!` , 'green');
+    }
+    else{
+        guessesLeft -=1;
+        if (guessesLeft === 0){
+            guessInput.disabled = true;
+        guessInput.style.borderColor = 'red';
+        setMessage(`Game Over, you lost! the correct number was ${winningNum}`);
+        }else{
+            guessInput.style.borderColor = 'red';
+            guessInput.value = '';
+            setMessage(`${enteredNum} is not correct! ${guessesLeft} guesses left` , 'red');
+        }
+    }
 }
 
-  guessBtn.addEventListener('click' , getNumber)
+  guessBtn.addEventListener('click' , numberCheck)
