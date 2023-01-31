831f683a94429c83bc3844c884438b11c5bf9ece
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-31 11:57:28 +0100

    made a error box in the html using UI constractor

--- book-list/app.js
+++ book-list/app.js
@@ -13,6 +13,19 @@ UI.prototype.addBookToLIst = function (book) {
   row.innerHTML = `<td>${book.title}</td> <td>${book.author}</td> <td>${book.isbn}</td> <td> <a href="#" class="delete">X</a> </td>`;
   list.appendChild(row);
 };
+UI.prototype.clearFields = function () {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+UI.prototype.showAlert = function(message , className){
+const div = document.createElement('div');
+div.className=`alert ${className}`;
+div.appendChild(document.createTextNode(message));
+const container = document.querySelector('.container');
+const form = document.querySelector('#book-form');
+container.insertBefore(div, form);
+}
 
 //event listeners
 document.getElementById("book-form").addEventListener("submit", function (e) {
@@ -23,8 +36,16 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
 
   //instantiate book
   const book = new Book(title, author, isbn);
-
-  //instantiate UI
   const ui = new UI();
-  ui.addBookToLIst(book);
+
+  //validate form
+  if (title === "" || author === "" || isbn === "") {
+   ui.showAlert('Please fill in all inputs!' , 'error');
+  } else {
+    //instantiate UI
+    ui.addBookToLIst(book);
+
+    //clear form
+    ui.clearFields();
+  }
 });
