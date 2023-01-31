e16071394d7bba35c13e25b4bbe966c99e9dc5dd
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-31 11:25:54 +0100

    create a prototype for UI constructor to insert a book in the UI.

--- book-list/app.js
+++ book-list/app.js
@@ -1,22 +1,30 @@
-
 //book constructor
-function Book (title , author , isbn){
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
 }
 
 //UI constructor
 function UI() {}
+UI.prototype.addBookToLIst = function (book) {
+  const list = document.getElementById("book-list");
+  const row = document.createElement("tr");
+  row.innerHTML = `<td>${book.title}</td> <td>${book.author}</td> <td>${book.isbn}</td> <td> <a href="#" class="delete">X</a> </td>`;
+  list.appendChild(row);
+};
 
 //event listeners
-document.getElementById('book-form').addEventListener('submit' , function(e){
-    const title = document.getElementById('title').value , 
-    author = document.getElementById('author').value , 
-    isbn = document.getElementById('isbn').value;
-    e.preventDefault();
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+  e.preventDefault();
 
-    //instantiate book
-    const book = new Book(title , author , isbn);
-    console.log(book)
-} )
+  //instantiate book
+  const book = new Book(title, author, isbn);
+
+  //instantiate UI
+  const ui = new UI();
+  ui.addBookToLIst(book);
+});
