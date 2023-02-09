c921314532755bd6f68437fc30276041325e690e
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-02-09 11:14:28 +0100

    make a function to change the location and show its current weather.

--- weatherForecast/app.js
+++ weatherForecast/app.js
@@ -1,28 +1,31 @@
 const weather = new Weather("ahvaz");
+const ui = new UI();
 
-
-// setTimeout(()=>{
-//     weather.changeLocation('shiraz')
-//     console.log('shiraz')
-// } , 5000)
-
-const allEventListeners = ()=>{
-    document.addEventListener("DOMContentLoaded", getWeather());
-}
-
+const allEventListeners = () => {
+  document.addEventListener("DOMContentLoaded", getWeather());
+  document
+    .getElementById("w-change-btn")
+    .addEventListener("click", newLocationRequest);
+};
 
 const getWeather = () => {
   weather
     .getWeather()
     .then((result) => {
       console.log(result);
-      const ui = new UI();
-      ui.paint(result)
+      ui.paint(result);
     })
     .catch((error) => {
       console.log("something went wrong!", error);
     });
 };
 
+const newLocationRequest = () => {
+  const city = document.getElementById("city").value;
+  weather.changeLocation(city);
+  getWeather();
+  //To close the modal
+  $("#locModal").modal("hide");
+};
 
 allEventListeners();
--- weatherForecast/ui.js
+++ weatherForecast/ui.js
@@ -1,28 +1,26 @@
-class UI{
-    constructor(){
-        this.location = document.getElementById('w-location');
-        this.desc = document.getElementById('w-desc');
-        this.string = document.getElementById('w-string');
-        this.details = document.getElementById('w-details');
-        this.icon = document.getElementById('w-icon');
-        this.humidity = document.getElementById('w-humidity');
-        this.feelsLike = document.getElementById('w-feels-like');
-        this.windSpeed = document.getElementById('w-wind-speen');
-        this.pressure = document.getElementById('w-pressure');
-    }
-    paint(weather){
-        this.location.textContent = weather.location.name;
-        this.desc.textContent= weather.current.condition.text;
-        this.string.textContent= weather.current.temp_c;
-        this.icon.setAttribute('src' , weather.current.condition.icon);
-        this.humidity.textContent = `Relative Humidity: ${weather.current.humidity}`;
-        this.feelsLike.textContent= `Feels Like: ${weather.current.feelslike_c}`;
-        this.windSpeed.textContent = `Wind Speed: ${weather.current.wind_kph}`;
-        this.pressure.textContent = `Pressure in inches: ${weather.current.pressure_in}`;
+class UI {
+  constructor() {
+    this.location = document.getElementById("w-location");
+    this.desc = document.getElementById("w-desc");
+    this.string = document.getElementById("w-string");
+    this.details = document.getElementById("w-details");
+    this.icon = document.getElementById("w-icon");
+    this.humidity = document.getElementById("w-humidity");
+    this.feelsLike = document.getElementById("w-feels-like");
+    this.windSpeed = document.getElementById("w-wind-speen");
+    this.pressure = document.getElementById("w-pressure");
+    this.degreeSymbol = document.getElementById("w-degreeSymbol");
+  }
+  paint(weather) {
+    this.location.textContent = weather.location.name;
+    this.desc.textContent = weather.current.condition.text;
+    this.string.textContent = weather.current.temp_c;
+    this.icon.setAttribute("src", weather.current.condition.icon);
+    this.humidity.textContent = `Relative Humidity: ${weather.current.humidity}`;
+    this.feelsLike.textContent = `Feels Like: ${weather.current.feelslike_c}`;
+    this.windSpeed.textContent = `Wind Speed: ${weather.current.wind_kph}`;
+    this.pressure.textContent = `Pressure in inches: ${weather.current.pressure_in}`;
 
-
-
-
-
-    }
+    this.degreeSymbol.innerHTML = "&#8451";
+  }
 }
